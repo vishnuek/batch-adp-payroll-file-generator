85c990c8f33a0e5b2678f49dfd0fbc7027267ffe
Vishnu E K <vearathputhenveedu@mbopartners.com>
2023-02-07 13:33:51 +0530

    initial commit

---
+++ src/main/java/com/mbopartners/payroll/BatchAdpPayrollFileGeneratorApplication.java
@@ -0,0 +1,13 @@
+package com.mbopartners.payroll;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BatchAdpPayrollFileGeneratorApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BatchAdpPayrollFileGeneratorApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/mbopartners/payroll/BatchAdpPayrollFileGeneratorApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mbopartners.payroll;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BatchAdpPayrollFileGeneratorApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
